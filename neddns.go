90ac00fda7874490bbff4086c347f55546549661
ned <ned@camelspit.org>
2015-11-15 12:51:52 -0700

    Add StatsD support

--- neddns.go
+++ neddns.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/docopt/docopt-go"
 	"github.com/miekg/dns"
+	"github.com/quipo/statsd"
 	"io"
 	"io/ioutil"
 	"log"
@@ -17,7 +18,7 @@ import (
 	"time"
 )
 
-var version = "0.1-20150904"
+var version = "0.1.2015111500"
 
 var usage = `neddns: simple authoratative DNS server backed by S3
 
@@ -39,6 +40,8 @@ Options:
   -f, --prefix=<prefix>     AWS object prefix (such as directory name).
   -r, --resolver=<host:port>	DNS resolver for CNAME flattening [default: 8.8.8.8:53].
   -l, --log=<path>          Write to file at this loctation rather than stdout.
+  --statsd_server=<host:port>	Statsd server and port - statsd is disabled if empty.
+  --statsd_prefix=<prefix>		Prefix to add to statsd metrics [default: neddns].
   -d, --debug               Enable debugging output.
   -h, --help                Show this screen.
   --version                 Show version.
@@ -50,17 +53,20 @@ type zone struct {
 }
 
 type config struct {
-	awsKeyId   string
-	awsSecret  string
-	bucket     string
-	port       string
-	logfile    string
-	region     string
-	prefix     string
-	resolver   string
-	debugOn    bool
-	lastUpdate time.Time
-	update     time.Duration
+	awsKeyId     string
+	awsSecret    string
+	bucket       string
+	port         string
+	logfile      string
+	region       string
+	prefix       string
+	resolver     string
+	debugOn      bool
+	lastUpdate   time.Time
+	update       time.Duration
+	statsdServer string
+	statsdPrefix string
+	stats        statsd.Statsd
 }
 
 func main() {
@@ -77,6 +83,13 @@ func main() {
 		defer logfile.Close()
 		log.SetOutput(logfile)
 	}
+	if len(c.statsdServer) > 0 {
+		c.stats = statsd.NewStatsdClient(c.statsdServer, c.statsdPrefix)
+		c.stats.CreateSocket()
+		c.debug("Statsd enabled.")
+	} else {
+		c.stats = statsd.NoopClient{}
+	}
 
 	getter := s3getter{region: c.region, bucket: c.bucket, prefix: c.prefix}
 	c.debug("Fetching zones...")
@@ -84,6 +97,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	c.stats.Gauge("zones", int64(len(z)))
 	c.debug(fmt.Sprintf("Fetched %d zones...", len(z)))
 
 	c.debug("Loading zones...")
@@ -91,9 +105,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	c.registerVersionHandler()
 	c.debug("Starting server...")
-	startServer(c.port)
+	c.startServer()
 	log.Printf("DNS server running on TCP/UDP port %s (v%s)", c.port, version)
+	c.stats.Incr("started", 1)
 
 	doUpdate := make(chan bool)
 	go func() {
@@ -110,6 +126,7 @@ func main() {
 			}
 			c.debug(fmt.Sprintf("Fetched %d updated zones", len(z)))
 			if len(z) > 0 {
+				c.stats.Incr("zoneupdates", int64(len(z)))
 				c.debug(fmt.Sprintf("Reloading %d zones now", len(z)))
 				err = c.loadZones(z)
 				if err != nil {
@@ -191,6 +208,7 @@ func (c *config) loadZones(zones map[string]string) error {
 }
 
 func (z *zone) zoneHandler(c *config, w dns.ResponseWriter, req *dns.Msg) {
+	c.stats.Incr("query.request", 1)
 	m := new(dns.Msg)
 	m.SetReply(req)
 	m.Authoritative = true
@@ -198,12 +216,14 @@ func (z *zone) zoneHandler(c *config, w dns.ResponseWriter, req *dns.Msg) {
 	questions := []string{}
 	answers := []string{}
 	if len(req.Question) != 1 {
+		c.stats.Incr("query.error", 1)
 		log.Printf("Warning: len(req.Question) != 1")
 		return
 	}
 	q := req.Question[0]
 	questions = append(questions, fmt.Sprintf("%s[%s]", q.Name, dns.TypeToString[q.Qtype]))
 	if q.Qclass != uint16(dns.ClassINET) {
+		c.stats.Incr("query.error", 1)
 		log.Printf("Warning: skipping unhandled class: %s", dns.ClassToString[q.Qclass])
 		return
 	}
@@ -235,6 +255,7 @@ func (z *zone) zoneHandler(c *config, w dns.ResponseWriter, req *dns.Msg) {
 	//m.Extra = []dns.RR{}
 	//m.Extra = append(m.Extra, &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"DNS rocks"}})
 	c.debug(fmt.Sprintf("Query [%s] %s -> %s ", w.RemoteAddr().String(), strings.Join(questions, ","), strings.Join(answers, ",")))
+	c.stats.Incr("query.answer", 1)
 
 	w.WriteMsg(m)
 }
@@ -264,8 +285,7 @@ func (c *config) flattenCNAME(in *dns.CNAME) ([]dns.RR, error) { // TODO: cache 
 	return answers, nil
 }
 
-func startServer(port string) {
-	// special handler for reporting version: dig . @host TXT
+func (c *config) registerVersionHandler() { // special handler for reporting version: dig . @host TXT
 	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
 		m := new(dns.Msg)
 		m.SetReply(req)
@@ -278,16 +298,18 @@ func startServer(port string) {
 		}
 		w.WriteMsg(m)
 	})
+}
 
+func (c *config) startServer() {
 	go func() {
-		srv := &dns.Server{Addr: ":" + port, Net: "udp"}
+		srv := &dns.Server{Addr: ":" + c.port, Net: "udp"}
 		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatalf("Failed to set udp listener %s\n", err.Error())
 		}
 	}()
 	go func() {
-		srv := &dns.Server{Addr: ":" + port, Net: "tcp"}
+		srv := &dns.Server{Addr: ":" + c.port, Net: "tcp"}
 		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatalf("Failed to set tcp listener %s\n", err.Error())
@@ -333,6 +355,17 @@ func parseArgs() (config, error) {
 	}
 	if len(c.awsKeyId) < 1 || len(c.awsSecret) < 1 {
 		return c, fmt.Errorf("Must use -K and -S options or set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.")
+	}
+	if arg, ok := args["--statsd_server"].(string); ok {
+		c.statsdServer = arg
+	}
+	if arg, ok := args["--statsd_prefix"].(string); ok {
+		c.statsdPrefix = arg
+		if !strings.HasSuffix(c.statsdPrefix, ".") {
+			c.statsdPrefix += "."
+		}
+	} else {
+		c.statsdPrefix = "neddns."
 	}
 	return c, nil
 }
